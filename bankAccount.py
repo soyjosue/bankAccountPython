d8ea6d02e2b3dd54f230565204f1e3f7b399e1c6
Elvis Josue Inoa Santiago <66582266+soyjosue@users.noreply.github.com>
2024-08-17 18:54:02 -0400

    Add files via upload

---
+++ bankAccount.py
@@ -0,0 +1,34 @@
+class BankAccount:
+    def __init__(self, account_holder, balance):
+        self.account_holder = account_holder
+        self.balance = balance
+        self.is_active = True
+
+    def deposit(self, amount):
+        if self.is_active:
+            self.balance += amount
+            print(f"Se ha depositado {amount}. Saldo actual {self.balance}")
+        else:
+            print("No se puede depositar, Cuenta inactiva")
+    def withdraw(self, amount):
+        if self.is_active:
+            if amount <= self.balance:
+                self.balance -= amount
+                print(f"Se ha retirado {amount}. Saldo actual {self.balance}")
+    def desactivate_account(self):
+        self.is_active = False
+        print(f"La cuenta ha sido desactivada.")
+    def activate_account(self):
+        self.is_active = False
+        print(f"La cuenta ha sido activada.")
+
+account1 = BankAccount("Ana", 500)
+account2 = BankAccount("Luis", 1000)
+
+# LLamada a los métodos
+account1.deposit(200)
+account2.deposit(100)
+account1.desactivate_account()
+account1.deposit(50)
+account1.activate_account()
+account1.deposit(50)
